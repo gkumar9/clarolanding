0c1a4d83630fa5f271830cc7fd9682ea469276e1
gkumar9 <gaurav9904@gmail.com>
2019-05-24 10:07:24 +0530

    working on changes

--- src/slider.js
+++ src/slider.js
@@ -2,13 +2,13 @@ import React, { Component } from "react";
 import { Jumbotron, Carousel, Container } from "react-bootstrap";
 // import spice from "./imgs/img2/new.png";
 import "./App.css";
-import test from "./imgs/img2/Image2.JPG";
-import shop from "./imgs/img2/shop.jpg";
-import haldi from "./imgs/img2/haldi.jpg";
-import irrigation from "./imgs/img2/irrigation.jpg";
-import shopmobile from "./imgs/img2/shopmobile.jpg";
-import haldimobile from "./imgs/img2/haldimobile.jpg";
-import irrigationmobile from "./imgs/img2/irrigationmobile.jpg";
+import claro from "./imgs/img2/claro.JPG";
+import pump from "./imgs/img2/pump.jpg";
+// import haldi from "./imgs/img2/haldi.jpg";
+// import irrigation from "./imgs/img2/irrigation.jpg";
+// import shopmobile from "./imgs/img2/shopmobile.jpg";
+// import haldimobile from "./imgs/img2/haldimobile.jpg";
+// import irrigationmobile from "./imgs/img2/irrigationmobile.jpg";
 class Slider extends Component {
   constructor(props, context) {
     super(props, context);
@@ -30,26 +30,26 @@ class Slider extends Component {
       direction: e.direction
     });
   }
-  componentDidMount() {
-    this.setState({
-      shopimg: shop,
-      irrigationimg: irrigation,
-      haldiimg: haldi
-    });
-    if (window.screen.width <= 480) {
-      this.setState({
-        shopimg: shopmobile,
-        irrigationimg: irrigationmobile,
-        haldiimg: haldimobile
-      });
-    } else {
-      this.setState({
-        shopimg: shop,
-        irrigationimg: irrigation,
-        haldiimg: haldi
-      });
-    }
-  }
+  // componentDidMount() {
+  //   this.setState({
+  //     shopimg: shop,
+  //     irrigationimg: irrigation,
+  //     haldiimg: haldi
+  //   });
+  //   if (window.screen.width <= 480) {
+  //     this.setState({
+  //       shopimg: shopmobile,
+  //       irrigationimg: irrigationmobile,
+  //       haldiimg: haldimobile
+  //     });
+  //   } else {
+  //     this.setState({
+  //       shopimg: shop,
+  //       irrigationimg: irrigation,
+  //       haldiimg: haldi
+  //     });
+  //   }
+  // }
   render() {
     const { index, direction } = this.state;
     return (
@@ -60,33 +60,25 @@ class Slider extends Component {
           onSelect={this.handleSelect}
           interval={1500}
         >
-        <Carousel.Item>
+          <Carousel.Item>
             <div className="imgtag">
-              <img
-                className="d-block w-100"
-                src={test}
-                alt="First slide"
-              />
+              <img className="d-block w-100" src={claro} alt="First slide" />
             </div>
 
             <Carousel.Caption>
               <h1>Fresh green produce from our farms</h1>
             </Carousel.Caption>
           </Carousel.Item>
-          {/* <Carousel.Item>
+          <Carousel.Item>
             <div className="imgtag">
-              <img
-                className="d-block w-100"
-                src={this.state.haldiimg}
-                alt="First slide"
-              />
+              <img className="d-block w-100" src={pump} alt="Second slide" />
             </div>
 
             <Carousel.Caption>
               <h1>Spices made from sustainable energy in Gangetic plains</h1>
             </Carousel.Caption>
           </Carousel.Item>
-          <Carousel.Item>
+          {/* <Carousel.Item>
             <div className="imgtag">
               <img
                 className="d-block w-100"
@@ -101,9 +93,7 @@ class Slider extends Component {
                 India
               </h1>
             </Carousel.Caption>
-          </Carousel.Item>
-           */}
-          
+          </Carousel.Item> */}
         </Carousel>
       </Jumbotron>
     );
